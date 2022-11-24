3cd986c871db0c2c98355d1c7433c2fc213d0fb1
JungMin <cvy5375@naver.com>
2022-11-24 17:25:41 +0900

    연산자

---
+++ operator.js
@@ -0,0 +1,12 @@
+const shirtsPrice = 100000
+const shoesPrice = 80000
+let totalPrice = 0
+
+totalPrice += shirtsPrice
+console.log(totalPrice)
+
+totalPrice += shoesPrice
+console.log(totalPrice)
+
+totalPrice -= shirtsPrice
+console.log(totalPrice)
