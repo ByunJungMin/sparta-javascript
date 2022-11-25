47b2c18c5389794b4e4a3a74a797337fb2efefea
JungMin <cvy5375@naver.com>
2022-11-25 15:17:28 +0900

    for 문

--- loop.js
+++ loop.js
@@ -1,5 +1,3 @@
-let temperature = 20
-while (temperature < 25) {
-    console.log(`${temperature} 도 정도면 적당한 온도입니다.`)
-    temperature++
+for (let temperature = 20; temperature < 25; temperature++) {
+    console.log(`${temperature} 도 정도면 적당한 온도 입니다.`)
 }
