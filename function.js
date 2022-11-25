f443bf264f855f72687002eee8ff40e9829b0e8a
JungMin <cvy5375@naver.com>
2022-11-25 16:20:29 +0900

    function 1

--- function.js
+++ function.js
@@ -1,16 +1,11 @@
-function calculateAvg(price1, price2) {
-    const sum = price1 + price2
-    console.log(`두 상품의 가격 총합은 ${sum}입니다.`)
-    const avg = sum / 2
+function calculateAvg(price1, price2, price3) {
+    const avg = (price1 + price2 + price3) / 3
     return avg
 }
 
 const priceA = 1000
 const priceB = 2000
-const avg1 = calculateAvg(priceA, priceB)
-console.log(`A와 B의 평균은${avg1}입니다.`)
+const priceC = 3000
+const avg = calculateAvg(priceA, priceB, priceC)
+console.log(`평균: ${avg}`)
 
-const priceC = 2000
-const priceD = 3000
-const avg2 = calculateAvg(priceC, priceD)
-console.log(`A와 B의 평균은${avg2}입니다.`)
