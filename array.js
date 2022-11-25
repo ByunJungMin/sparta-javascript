a035a09cc81738321403f418934f141d4a209114
JungMin <cvy5375@naver.com>
2022-11-25 16:50:19 +0900

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+console.log(rainbowColors[0])
+console.log(rainbowColors[1])
+console.log(rainbowColors[2])
+console.log(rainbowColors[3])
+console.log(rainbowColors[4])
+console.log(rainbowColors[5])
+console.log(rainbowColors[6])
+
+console.log(rainbowColors.length)
+console.log(rainbowColors[rainbowColors.length - 1])  // 배열의 마지막 
