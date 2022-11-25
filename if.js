415ada64fd79947abc3b10689d68b3d4ef96d941
JungMin <cvy5375@naver.com>
2022-11-25 15:11:19 +0900

    if, else if, else

--- if.js
+++ if.js
@@ -1,9 +1,9 @@
-const shoesPrice = 40000
-if ( shoesPrice < 50000 ) {
-    console.log('이 신발을 사겠습니다.')
-}
+const distance = 2
 
-const capPrice = 50000
-if ( capPrice < 50000 ) {
-    console.log('이 모자를 사겠습니다')
+if (distance < 2) {
+    console.log('걸어가자')
+} else if (distance >= 2 && distance < 5) {
+    console.log('택시를 타자')
+} else {
+    console.log('기차를 타자')
 }
